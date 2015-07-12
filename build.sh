a36564557cec8e4953c9828d01155f65caf42c15
Nils Bars <nils.bars@haw-hamburg.de>
2015-07-12 13:51:08 +0200

    Add initrd and update build script

--- build.sh
+++ build.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-
 toolchain_dir=$(pwd)/toolchain
 build_dir=$(pwd)/kernel-src
 toolchain_version="arm-eabi-4.6"
 arm__build_toolchain_url="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/${toolchain_version}"
 
-#Toolchain setup
-path=$(which arm-eabi-gcc)
-if [[ -n "$path" ]]; then
-  echo ""
-  echo "Found arm-eabi toolchain in your path, no need to download"
+output_dir=$(pwd)/out
+
+if [[ -e "${toolchain_dir}/${toolchain_version}/bin/arm-eabi-gcc" ]]; then
+  echo "Toolchain was already downloaded"
 else
-  if [[ -n "${toolchain_dir}/${toolchain_version}/bin/arm-eabi-gcc" ]]; then
-    echo "Toolchain was already downloaded"
-  else
-    mkdir -p $toolchain_dir
-    cd $toolchain_dir
+  mkdir -p $toolchain_dir
+  cd $toolchain_dir
 
-    echo "Cloning ARM build toolchain from ${arm__build_toolchain_url}"
-    git clone $arm__build_toolchain_url
-    echo "Cloning finished!"
-  fi
-
+  echo "Cloning ARM build toolchain from ${arm__build_toolchain_url}"
+  git clone $arm__build_toolchain_url
+  echo "Cloning finished!"
 fi
 
 cross_compile="${toolchain_dir}/${toolchain_version}/bin/arm-eabi-"
@@ -33,5 +26,13 @@ export ARCH=arm
 
 #Building
 cd $build_dir
+#make clean
 make n1awifi_00_defconfig
 make -j5
+
+#Move zIamge
+mkdir -p $output_dir
+echo "Copying zIamge to ${output_dir}"
+cp $build_dir/arch/arm/boot/zImage $output_dir/
+
+#Building initrd
