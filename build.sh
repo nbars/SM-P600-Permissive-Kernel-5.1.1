f1ce47a408f2e1c933f401117035a2d8376e9bc3
Nils Bars <nils.bars@haw-hamburg.de>
2015-07-24 11:36:44 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@ toolchain_dir=$(pwd)/toolchain
 build_dir=$(pwd)/kernel-src
 toolchain_version="arm-eabi-4.6"
 arm__build_toolchain_url="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/${toolchain_version}"
+initrd_path=$(pwd)/initrd
 
 output_dir=$(pwd)/out
+out_zimage=$output_dir/zImage
+out_initrd=$output_dir/initramfs.cpio.gz
+out_boot_image=$output_dir/boot.img
+out_odin_pda=$output_dir/boot.tar
 
 if [[ -e "${toolchain_dir}/${toolchain_version}/bin/arm-eabi-gcc" ]]; then
   echo "Toolchain was already downloaded"
@@ -30,9 +35,25 @@ cd $build_dir
 make n1awifi_00_defconfig
 make -j5
 
+rm -rf $output_dir/*
+
 #Move zIamge
 mkdir -p $output_dir
-echo "Copying zIamge to ${output_dir}"
-cp $build_dir/arch/arm/boot/zImage $output_dir/
+echo "Copying zIamge to ${out_zimage}"
+cp -f $build_dir/arch/arm/boot/zImage $out_zimage
 
 #Building initrd
+echo "Creating initrd at ${out_initrd}"
+cd $initrd_path
+find . | cpio -o -H newc | gzip > $out_initrd
+
+cd $output_dir
+
+echo "Creating boot image ${out_boot_image}"
+mkbootimg --kernel $(basename $out_zimage) --ramdisk $(basename $out_initrd) -o $out_boot_image
+
+echo "Creating odin PDA file"
+tar -H ustar -c $(basename $out_boot_image) > $(basename $out_odin_pda)
+
+md5sum $(basename $out_odin_pda) >> $(basename $out_odin_pda)
+mv $out_odin_pda $out_odin_pda.md5
